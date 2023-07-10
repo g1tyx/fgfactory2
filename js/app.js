4317d5ae8bc9fcb98f7d6ea99b3eb3902b8a97cd
Freddec <freddec.games@gmail.com>
2023-07-10 10:33:52 +0200

    dev

--- js/app.js
+++ js/app.js
@@ -66,13 +66,7 @@ class App {
                 //---
                 if (loadedData.game) this.game.load(loadedData.game)
             }
-            else {
-                //---
-                this.game.loadScenario('tut')
-                this.game.scenario.startDate = Date.now()
-                //---
-                this.game.refreshUnlocked()
-            }
+            else this.game.start('tut')
             //---            
             window.onbeforeunload = () => {
                 //---
@@ -118,12 +112,10 @@ class App {
             //---
             if (this.game.isVictoryReached()) {
                 //---
-                this.game.victory = true
+                this.game.doVictory()
+                //---
                 this.showModal('modalVictory')
-                //---
-                this.game.scenario.victoryDate = Date.now()
-                //---
-                this.screens['game'].refreshTabScenarii()
+                this.screens['game'].refreshTabScenarios()
             }
             //---
             this.screens['game'].draw()
